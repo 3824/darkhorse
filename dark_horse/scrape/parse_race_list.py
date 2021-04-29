a944783af01d8ec506193a82c40557a0a413a391
miya_home <miyanishikazunori@gmail.com>
2021-04-29 13:53:59 +0900

    add comment

--- dark_horse/scrape/parse_race_list.py
+++ dark_horse/scrape/parse_race_list.py
@@ -3,14 +3,21 @@ from bs4 import BeautifulSoup
 from urllib import request
 from urllib.parse import urljoin
 
+base_url = "https://db.netkeiba.com"
 
 def load_list(date_str):
     url = f"https://db.netkeiba.com/?pid=race_kaisai&date={date_str}"
     print(url)
     response = request.urlopen(url)
     bs = BeautifulSoup(response, "html.parser")
-    for item in bs.find_all("dl", class_="race_top_data_info fc"):
-        print(urljoin(url, item.select("a")[0].get('href')))
+    # for item in bs.find_all("dl", class_="race_top_data_info fc"):
+    #     print(urljoin(url, item.select("a")[0].get('href')))
+    central_pages = [urljoin(base_url, item.select("a")[0].get('href')) for item in bs.find_all("dl", class_="race_top_data_info fc")]
+
+    # 地方競馬の結果を取得したい
+    
+    return central_pages
 
 if __name__ == '__main__':
-    load_list("20210306")
+    print(load_list("20210306"))
+    print(load_list("20210305"))
